674037f6503852e1b86680b8d30f34cbd4041eff
mohmehdi <m.m.afkhami@outlook.com>
2021-09-06 23:21:18 +0430

    a basic movement and jump added with a dull friction and gravity #1

--- Assets/src/Actors/Actor.gd
+++ Assets/src/Actors/Actor.gd
@@ -1,2 +1,10 @@
 extends KinematicBody2D
 class_name Actor
+
+const FLOOR_NORMAL := Vector2.UP
+
+export var gravity:= 300.0
+export var speed := Vector2(300,1000)
+var velocity := Vector2.ZERO
+
+	
--- Assets/src/Actors/Player.gd
+++ Assets/src/Actors/Player.gd
@@ -1,1 +1,28 @@
 extends Actor
+
+func _physics_process(delta):
+	var move_direction = get_direction()
+	velocity = calc_velocity(velocity,move_direction,speed)
+	velocity = move_and_slide(velocity,FLOOR_NORMAL)
+
+func get_direction():
+	return Vector2(
+		Input.get_action_strength("move_right")
+		-Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0)
+
+func calc_velocity(
+	current_velocity:Vector2 ,
+	direction:Vector2 , 
+	speed:Vector2):
+	
+	var new_velocity = Vector2(
+				current_velocity.x/1.2 ,
+				current_velocity.y +gravity * get_physics_process_delta_time())
+	if direction.y <= -0.9:
+		new_velocity.y = direction.y*speed.y
+	if abs(direction.x)> 0 and abs(new_velocity.x)< speed.x :
+		new_velocity.x += direction.x*speed.x
+	
+	return new_velocity
+	
